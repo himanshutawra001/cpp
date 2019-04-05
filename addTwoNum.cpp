9023540ca280b8d36b6227b293cc31ab64515e7b
himanshutawra001 <49098607+himanshutawra001@users.noreply.github.com>
2019-04-06 02:33:16 +0530

    Create addTwoNum.cpp

---
+++ addTwoNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int num1,num2,sum;
+cout<<"Enter two integers"<<endl;
+cin>>num1>>num2;
+
+sum=num1+num2;
+cout<<"Sum of "<<num1<<"and "<<num2<<" is: "<<sum<<endl;
+return 0;
+}
