ab2fa311b111aeab8ebeca3fa248094d91803c23
himanshutawra001 <49098607+himanshutawra001@users.noreply.github.com>
2019-04-06 19:44:31 +0530

    Create swapTwoNumbers.cpp

---
+++ swapTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a=10,b=15,temp;
+cout<<"Before swapping: "<<"a="<<a<<" b="<<b<<endl;
+
+temp=a;
+a=b;
+b=temp;
+cout<< "After swapping: "<<"a="<<a<<" b="<<b<<endl;
+return 0;
+}
